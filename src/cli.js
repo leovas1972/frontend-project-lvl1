9640647c515246e0f5a91798fbec99f67f500d62
Leontev Vasilii <leovas1972@gmail.com>
2022-07-04 09:41:35 +0700

    Created src/cli.js.Import readlineSync and created function question and implementation of its logic

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const question = () => {
+  const userName = readlineSync.question('May I have your name?');
+  return console.log(`Hello ${userName}!`);
+  ;
+}
+
+export default question;
