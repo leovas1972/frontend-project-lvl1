91ea9f4110c7cc4e8fd176ae45de3c2e36620c80
Leontev Vasilii <leovas1972@gmail.com>
2022-07-08 14:53:50 +0700

    changed to greetings in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const question = (str, el) => {
-  const userName = readlineSync.question(str);
-  return console.log(`${el} ${userName}!`);
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`'Hello' ${userName}!`);
+  return userName;
 };
 
-export default question;
+export default greetings;
