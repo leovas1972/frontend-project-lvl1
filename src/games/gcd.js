7fb36d50c7ca9b309a4af1b49452682dc2072f0e
Leontev Vasilii <leovas1972@gmail.com>
2022-07-09 23:27:40 +0700

    created gcd.js implementation runGcdGames

---
+++ src/games/gcd.js
@@ -0,0 +1,28 @@
+import getRandomInt from '../utils/getRandomInt.js'
+import runGames from '../index.js'
+
+const taskGame ='Find the greatest common divisor of given numbers.';
+function NOD () {
+  for (var x = arguments[0], i = 1; i < arguments.length; i++) {
+    var y = arguments[i];
+    while (x && y) {
+      x > y ? x %= y : y %= x;
+    }
+    x += y;
+  }
+  return x;
+}
+
+const toGetData = () => {
+  const number1 = getRandomInt(1,20);
+  const number2 = getRandomInt(1,61);
+  const correctAnswer = NOD(number1, number2);
+  const question = `Question: ${number1} ${number2}`;
+  return [question, correctAnswer];
+}
+
+const runGcdGames = () => {
+  runGames(taskGame, toGetData);
+};
+
+export default runGcdGames;
