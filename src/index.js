df63f9b70fbdb256b7e28af2e34263f85f918637
Leontev Vasilii <leovas1972@gmail.com>
2022-07-09 23:43:02 +0700

    removed from index.js console.log()

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const runGames = (taskGame,toGetData) => {
   for(let i = 1; i <= numOfQuestion; i +=1){
     const[question, correctAnswer] = toGetData();
     console.log(`Question: ${question}`);
-    console.log(correctAnswer);
     const answerUser = Number(readlineSync.question('Your answer:'));
     if(correctAnswer === answerUser){
       console.log('Correct!'); 
