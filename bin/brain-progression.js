ea0ef3256b266c6edffc2cf9b565276211298be8
Leontev Vasilii <leovas1972@gmail.com>
2022-07-11 11:36:12 +0700

    Created progression.js implementation runProgressionGames

---
+++ bin/brain-progression.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import runPregressionGames from '../src/games/progression.js';
+runPregressionGames();
---
+++ src/games/progression.js
@@ -0,0 +1,28 @@
+import getRandomInt from '../utils/getRandomInt.js'
+import runGames from '../index.js'
+const taskGame ='What number is missing in the progression?';
+const progressionGeneration = (random,step) => {
+  const result = [];
+  for(let i = 0; i < 10; i +=1){
+    let num = random + step * i;
+    result.push(num);
+  }
+  return result;
+}
+
+const toGetData = () => {
+  const random = getRandomInt(1,9);
+  const step = getRandomInt(1,9);
+  const arrayProgression = progressionGeneration(random,step);
+  const index = getRandomInt(1,arrayProgression.length - 1);
+  let correctAnswer = arrayProgression[index];
+  arrayProgression[index] = '..';
+  const question = arrayProgression.join(' ');
+  return [question, correctAnswer];
+}
+
+const runPregressionGames = () => {
+  runGames(taskGame, toGetData);
+};
+
+export default runPregressionGames;
