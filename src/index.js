8474778dfd8464bcdb3ba3256597875510bd2ee5
Leontev Vasilii <leovas1972@gmail.com>
2022-07-14 12:10:47 +0700

    redundant type casting String,readlineSunc in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const run = (description,generateRound) => {
   const roundsCount = 3;
-  
+
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello ${userName}!`);
@@ -11,7 +11,7 @@ const run = (description,generateRound) => {
   for(let i = 1; i <= roundsCount; i +=1){
     const[question, correctAnswer] = generateRound();
     console.log(`Question: ${question}`);
-    const answerUser = String(readlineSync.question('Your answer:'));
+    const answerUser = readlineSync.question('Your answer:');
     if(correctAnswer === answerUser){
       console.log('Correct!'); 
     } else {
