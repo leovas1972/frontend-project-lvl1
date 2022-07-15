d8025805842224121ece084de4c662240ce7f853
Leontev Vasilii <leovas1972@gmail.com>
2022-07-15 20:43:54 +0700

    created function indexSelection

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
 export const getRandomInt = (min, max) => Math.round(Math.random() * (max - min)) + min;
+
+export const indexSelection = (arr) => {
+  return arr[getRandomInt(0, arr.length - 1)];
+};
