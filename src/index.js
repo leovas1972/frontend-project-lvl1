d2df0090381dd0ad387184a464026b6d2eeee12b
Leontev Vasilii <leovas1972@gmail.com>
2022-07-15 18:44:07 +0700

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import readlineSync from 'readline-sync';
 
-const run = (description,generateRound) => {
-  const roundsCount = 3;
+const roundsCount = 3;
 
+const run = (description, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello ${userName}!`);
   console.log(description);
   
-  for(let i = 1; i <= roundsCount; i +=1){
-    const[question, correctAnswer] = generateRound();
+  for(let i = 0; i < roundsCount; i += 1){
+    const[question, answer] = generateRound();
     console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer:');
-    if(correctAnswer !== userAnswer){
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.
+    const answerUser = readlineSync.question('Your answer:');
+
+    if(answer !== answerUser){
+      console.log(`${answerUser} is wrong answer ;(. Correct answer was ${answer}.
       Let's try again, ${userName}!`);
       return; 
     }
