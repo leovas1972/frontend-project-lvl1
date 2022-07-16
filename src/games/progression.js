7ef38eb2be8f74c83099fc5c3ff89f5357d29f31
Leontev Vasilii <leovas1972@gmail.com>
2022-07-16 19:32:41 +0700

    refactoring progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,26 +1,31 @@
-import {getRandomInt} from '../utils.js'
-import run from '../index.js'
+import { getRandomInt, indexSelection } from '../utils.js';
+import run from '../index.js';
 
 const description ='What number is missing in the progression?';
-const progressionGeneration = (random,step) => {
+const lengthOfProgression = 10;
+
+const createProgression = (startNumber, step, lengthOfProgression) => {
   const result = [];
-  for(let i = 0; i < 10; i += 1){
-    let num = random + step * i;
+
+  for(let i = 0; i < lengthOfProgression; i += 1){
+    let num = startNumber + step * i;
     result.push(num);
   }
   return result;
-}
+};
 
 const generateRound = () => {
-  const random = getRandomInt(1,9);
-  const step = getRandomInt(1,9);
-  const arrayProgression = progressionGeneration(random,step);
-  const index = getRandomInt(1,arrayProgression.length - 1);
-  let answer = String(arrayProgression[index]);
-  arrayProgression[index] = '..';
-  const question = arrayProgression.join(' ');
+  const startNumber = getRandomInt(1, 70);
+  const step = getRandomInt(1, 9);
+
+  const resultingProgression = createProgression(startNumber, step, lengthOfProgression);
+  const hiddenIndex = resultingProgression.indexOf(indexSelection(resultingProgression));
+  const answer = String(resultingProgression[hiddenIndex]);
+  resultingProgression[hiddenIndex] = '..';
+  const question = resultingProgression.join(' ');
+
   return [question, answer];
-}
+};
 
 const runProgression = () => {
   run(description, generateRound);
