4b4670d56207eb5012bd31e94e711a64dac3aa3e
Leontev Vasilii <leovas1972@gmail.com>
2022-07-21 21:08:24 +0700

    refactoring progress.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,27 +2,26 @@ import { getRandomInt, getRandomIndex } from '../utils.js';
 import run from '../index.js';
 
 const description = 'What number is missing in the progression?';
+const minLength = 10;
 const minRange = 1;
 const maxRange = 70;
 const minStep = 1;
 const maxStep = 9;
 
-const createLength = (start, step, lengthOfProgression) => {
-  const result = [];
+const creatProgression = (start, step, length) => {
+  const progression = [];
 
-  for (let i = 0; i < lengthOfProgression; i += 1) {
-    const num = start + step * i;
-    result.push(num);
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + step * i);
   }
-  return result;
+  return progression;
 };
 
 const generateRound = () => {
-  const lengthOfProgression = 10;
   const start = getRandomInt(minRange, maxRange);
   const step = getRandomInt(minStep, maxStep);
 
-  const progression = createLength(start, step, lengthOfProgression);
+  const progression = creatProgression(start, step, minLength);
   const hiddenIndex = getRandomIndex(progression);
   const answer = String(progression[hiddenIndex]);
   progression[hiddenIndex] = '..';
